994b702550c1b32ab19fa7547023ec65c45d32da
Rares Andrei <36548850+rsh1337@users.noreply.github.com>
2022-04-26 19:40:43 +0300

    header

---
+++ components/layout.js
@@ -0,0 +1,115 @@
+import Head from "next/head";
+import Link from "next/link";
+import {
+  Box,
+  Heading,
+  Button,
+  Container,
+  useDisclosure,
+  HStack,
+  Stack,
+  Spacer,
+  VStack,
+  Grid,
+  InputGroup,
+  Input,
+  InputRightElement,
+  IconButton,
+} from "@chakra-ui/react";
+import { HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+function SearchBar() {
+  const router = useRouter();
+  const { terms } = router.query;
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    setText(terms || "");
+  }, [terms]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    if (text !== terms) {
+      router.push(`/search/?terms=${text}`, undefined, { shallow: true });
+    }
+  };
+
+  return (
+    <InputGroup as="form" onSubmit={handleSearch}>
+      <Input
+        variant="outline"
+        focusBorderColor="Lime"
+        placeholder="Search"
+        _placeholder={{ color: "inherit", opacity: 1 }}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+      <InputRightElement>
+        <IconButton aria-label="" icon={<SearchIcon />} type="submit" />
+      </InputRightElement>
+    </InputGroup>
+  );
+}
+
+const MenuItem = ({ href, children, ...props }) => (
+  <Link href={href} passHref>
+    <Button as="a" variant="link" {...props}>
+      {children}
+    </Button>
+  </Link>
+);
+
+function Header() {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Box>
+      <Container>
+        <Stack as="nav" direction={["column", , "row"]} wrap="wrap" py="1rem">
+          <Box display={["block", , "none"]} onClick={onToggle}>
+            <Button variant="outline">
+              <HamburgerIcon />
+            </Button>
+          </Box>
+
+          <Box display={[isOpen ? "block" : "none", , "block"]}>
+            <MenuItem href="/" mt={4}>
+              <Box size="sm" mr={3}>Login</Box>
+            </MenuItem>
+            <MenuItem href="/">
+              <Box size="sm" mr={3}>Register</Box>
+            </MenuItem>
+            <MenuItem href="/">
+              <Box size="sm">Favorite</Box>
+            </MenuItem>
+          </Box>
+          <Spacer />
+          <Box display={[isOpen ? "block" : "none", , "block"]}>
+            <SearchBar />
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Grid minH="100vh">
+        <VStack align="stretch" w="full" spacing={8}>
+          <Header />
+          <Box as="main" h="full">
+            {children}
+          </Box>
+        </VStack>
+      </Grid>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 import { Container } from "@chakra-ui/react";
 import CoinList from "../components/CoinList";
+import Layout from "../components/Layout";
 
-
-export default function Home({coinsData}) {
+export default function Home({ coinsData }) {
   return (
-    <Container>
-      <CoinList coinsData={coinsData}/>
-    </Container>
-  )
+    <Layout title="Crypto">
+      <Container>
+        <CoinList coinsData={coinsData} />
+      </Container>
+    </Layout>
+  );
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`)
-  const coinsData = await res.json()
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`
+  );
+  const coinsData = await res.json();
 
-  return { props: { coinsData } }
+  return { props: { coinsData } };
 }
