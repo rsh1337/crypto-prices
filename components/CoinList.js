7e0322718ff5383c30359e3e497cb047d06a9bf4
Rares Andrei <36548850+rsh1337@users.noreply.github.com>
2022-04-26 18:48:55 +0300

    done design

--- components/CoinList.js
+++ components/CoinList.js
@@ -1,24 +1,51 @@
-import { Box, Container, HStack, Image, Table, TableContainer, Tbody, Td, Th, Thead, Tr, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 
 const CoinList = ({ coinsData }) => {
   return (
     <TableContainer>
-      <Table>
+      <Table size="sm">
         <Thead>
           <Tr>
             <Th>Coin</Th>
             <Th isNumeric>Price</Th>
           </Tr>
         </Thead>
-        {coinsData.map(({ name, current_price, image, id }) => (
-          <Tbody key={id}>
-            <Tr>
-              <Td><Image borderRadius="full" boxSize="50px" src={image} alt={name} />
-              <Box>{name}</Box></Td>
-              <Td isNumeric><Box>${current_price}</Box></Td>
-            </Tr>
-          </Tbody>
-    ))}
+        {coinsData.map(
+          ({
+            name,
+            current_price,
+            image,
+            id,
+            price_change_percentage_24h_in_currency,
+          }) => (
+            <Tbody key={id}>
+              <Tr>
+                <Td>
+                  <Image
+                    borderRadius="full"
+                    boxSize="50px"
+                    src={image}
+                    alt={name}
+                  />
+                  <Box>{name}</Box>
+                </Td>
+                <Td isNumeric>
+                  <Box>${current_price}</Box>
+                </Td>
+              </Tr>
+            </Tbody>
+          )
+        )}
       </Table>
     </TableContainer>
   );
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({coinsData}) {
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
+  const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`)
   const coinsData = await res.json()
 
   return { props: { coinsData } }
