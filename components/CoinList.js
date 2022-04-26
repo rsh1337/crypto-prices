4cd7e69833fda7f0120b446cc902f2967f8a1050
Rares Andrei <36548850+rsh1337@users.noreply.github.com>
2022-04-26 18:06:56 +0300

    CoinList design

--- components/CoinList.js
+++ components/CoinList.js
@@ -1,16 +1,26 @@
-import { Box, Container, HStack, Image, VStack } from "@chakra-ui/react";
+import { Box, Container, HStack, Image, Table, TableContainer, Tbody, Td, Th, Thead, Tr, VStack } from "@chakra-ui/react";
 
 const CoinList = ({ coinsData }) => {
   return (
-    <Container>
-      {coinsData.map(({ name, current_price, image }) => (
-        <HStack spacing='24px'>
-            <Image borderRadius="full" boxSize="50px" src={image} alt={name} />
-            <Box>{name}</Box>
-            <Box>${current_price}</Box>
-        </HStack>
-      ))}
-    </Container>
+    <TableContainer>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Coin</Th>
+            <Th isNumeric>Price</Th>
+          </Tr>
+        </Thead>
+        {coinsData.map(({ name, current_price, image, id }) => (
+          <Tbody key={id}>
+            <Tr>
+              <Td><Image borderRadius="full" boxSize="50px" src={image} alt={name} />
+              <Box>{name}</Box></Td>
+              <Td isNumeric><Box>${current_price}</Box></Td>
+            </Tr>
+          </Tbody>
+    ))}
+      </Table>
+    </TableContainer>
   );
 };
 export default CoinList;
