bf6e9c1e9bf7750d88cb0bcd67731017e38bf169
Rares Andrei <36548850+rsh1337@users.noreply.github.com>
2022-05-07 09:52:30 +0300

    Signin With Google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import GoogleProvider from "next-auth/providers/google";
 const prisma = new PrismaClient()
 
 export default NextAuth({
@@ -13,6 +14,10 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
 });
